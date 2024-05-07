6b2771ade1bae65505521752c451143809821503
Jean-Michel Vedrine <vedrine@vedrine.net>
2024-05-07 20:50:34 +0200

    remove emoji from names

--- core/class/wled.class.php
+++ core/class/wled.class.php
@@ -166,6 +166,37 @@ class wled extends eqLogic {
         }
     }
 
+    public static function remove_emoji($string) {
+        // Match Enclosed Alphanumeric Supplement
+        $regex_alphanumeric = '/[\x{1F100}-\x{1F1FF}]/u';
+        $clear_string = preg_replace($regex_alphanumeric, '', $string);
+
+        // Match Miscellaneous Symbols and Pictographs
+        $regex_symbols = '/[\x{1F300}-\x{1F5FF}]/u';
+        $clear_string = preg_replace($regex_symbols, '', $clear_string);
+
+        // Match Emoticons
+        $regex_emoticons = '/[\x{1F600}-\x{1F64F}]/u';
+        $clear_string = preg_replace($regex_emoticons, '', $clear_string);
+
+        // Match Transport And Map Symbols
+        $regex_transport = '/[\x{1F680}-\x{1F6FF}]/u';
+        $clear_string = preg_replace($regex_transport, '', $clear_string);
+        
+        // Match Supplemental Symbols and Pictographs
+        $regex_supplemental = '/[\x{1F900}-\x{1F9FF}]/u';
+        $clear_string = preg_replace($regex_supplemental, '', $clear_string);
+
+        // Match Miscellaneous Symbols
+        $regex_misc = '/[\x{2600}-\x{26FF}]/u';
+        $clear_string = preg_replace($regex_misc, '', $clear_string);
+
+        // Match Dingbats
+        $regex_dingbats = '/[\x{2700}-\x{27BF}]/u';
+        $clear_string = preg_replace($regex_dingbats, '', $clear_string);
+
+        return $clear_string;
+    }
     /*
      * Fonction exécutée automatiquement toutes les 5 minutes par Jeedom
       public static function cron5() {
@@ -713,7 +744,7 @@ class wled extends eqLogic {
         $effects = array();
         foreach ($result as $k => $name) {
             if ($name != 'RSVD' && $name != "-") {
-                $effects[] = $k . '|' . $name;
+                $effects[] = $k . '|' . wled::remove_emoji($name);
             }
         }
         $listEffects = implode(';', $effects);
@@ -730,7 +761,7 @@ class wled extends eqLogic {
         $palettes = array();
         foreach ($result as $k => $name) {
             if ($name != 'RSVD' && $name != "-") {
-                $palettes[] = $k . '|' . $name;
+                $palettes[] = $k . '|' . wled::remove_emoji($name);
             }
         }
         $listPalettes = implode(';', $palettes);
