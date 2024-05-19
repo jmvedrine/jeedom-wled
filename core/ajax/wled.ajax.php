ccb8180a10cc91f0c32fbc21915caa8be0e70858
Mips2648 <mips2648@gmail.com>
2024-05-19 08:19:46 +0200

    intend with 4 spaces

--- core/ajax/wled.ajax.php
+++ core/ajax/wled.ajax.php
@@ -17,26 +17,26 @@
  */
 
 try {
-  require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
-  include_file('core', 'authentification', 'php');
+    require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+    include_file('core', 'authentification', 'php');
 
-  if (!isConnect('admin')) {
-    throw new Exception(__('401 - Accès non autorisé', __FILE__));
-  }
+    if (!isConnect('admin')) {
+        throw new Exception(__('401 - Accès non autorisé', __FILE__));
+    }
 
-  /* Fonction permettant l'envoi de l'entête 'Content-Type: application/json'
+    /* Fonction permettant l'envoi de l'entête 'Content-Type: application/json'
     En V3 : indiquer l'argument 'true' pour contrôler le token d'accès Jeedom
     En V4 : autoriser l'exécution d'une méthode 'action' en GET en indiquant le(s) nom(s) de(s) action(s) dans un tableau en argument
   */
-  ajax::init();
+    ajax::init();
 
-  if (init('action') == 'discoverWled') {
-    wled::discoverDevices();
-    ajax::success();
-  }
+    if (init('action') == 'discoverWled') {
+        wled::discoverDevices();
+        ajax::success();
+    }
 
-  throw new Exception(__('Aucune méthode correspondante à : ', __FILE__) . init('action'));
-  /*     * *********Catch exeption*************** */
+    throw new Exception(__('Aucune méthode correspondante à : ', __FILE__) . init('action'));
+    /*     * *********Catch exeption*************** */
 } catch (Exception $e) {
-  ajax::error(displayException($e), $e->getCode());
+    ajax::error(displayException($e), $e->getCode());
 }
